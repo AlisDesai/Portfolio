e886da2ba8e397c60a7c1f26581c6797932dc095
Alis Desai <aliskdesai111@email.com>
2026-07-21 10:34:37 +0530

    c

--- config/env.ts
+++ config/env.ts
@@ -4,7 +4,6 @@ const requiredEnv = {
 
 export const env = {
   siteUrl: requiredEnv.siteUrl,
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? "",
   isProduction: process.env.NODE_ENV === "production",
   isDevelopment: process.env.NODE_ENV === "development",
 } as const;
